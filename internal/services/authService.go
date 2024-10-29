aa376de31df67a87c534bdcb9ea9860bf41016af
Christian Isingizwe <bizimanaisingizwechristian@gmail.com>
2024-10-29 13:34:32 +0200

    Making the access and refresh tokens

--- internal/services/authService.go
+++ internal/services/authService.go
@@ -1,14 +1,18 @@
 package services
 
 import (
-	"errors"
-	"go/token"
 	"os"
 	"time"
 
 	"github.com/dgrijalva/jwt-go"
 	"golang.org/x/crypto/bcrypt"
 )
+
+
+var accessTokenSecretKey= os.Getenv("ACCESS_TOKEN_SECRET_KEY")
+var refreshTokenSecretKey= os.Getenv("REFRESH_TOKEN_SECRET_KEY")
+
+
 
 func HashPassword(password string) (string, error) {
 	bytes, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
@@ -24,7 +28,25 @@ func CheckPassword(hashedPassword, password string)error{
 }
 
 
-func GenerateAccessToken(userID string)(string, error)
+func GenerateAccessToken(userID string)(string, error){
+	claims := jwt.MapClaims{
+		"userId": userID,
+		"exp": time.Now().Add(30 * time.Minute).Unix(),
+	}
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+	return token.SignedString([]byte(accessTokenSecretKey))
+}
+
+func GenerateRefreshToken(userID string) (string, error){
+	claims := jwt.MapClaims{
+		"userId": userID,
+		"exp": time.Now().Add(7*24*time.Hour).Unix(),
+	}
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+	return token.SignedString([]byte(refreshTokenSecretKey))
+}
 
 
 // func ParseToken(tokenStr string) (uint, error){
