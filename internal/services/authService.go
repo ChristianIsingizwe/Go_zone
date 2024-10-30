15d711ad1d57cf79bd58b7a6347fc7aff6002bf0
Christian Isingizwe <bizimanaisingizwechristian@gmail.com>
2024-10-30 05:11:41 +0200

    Some changes made

--- internal/services/authService.go
+++ internal/services/authService.go
@@ -9,69 +9,38 @@ import (
 	"golang.org/x/crypto/bcrypt"
 )
 
-
-var accessTokenSecretKey= os.Getenv("ACCESS_TOKEN_SECRET_KEY")
-var refreshTokenSecretKey= os.Getenv("REFRESH_TOKEN_SECRET_KEY")
-
-
+var accessTokenSecretKey = os.Getenv("ACCESS_TOKEN_SECRET_KEY")
+var refreshTokenSecretKey = os.Getenv("REFRESH_TOKEN_SECRET_KEY")
 
 func HashPassword(password string) (string, error) {
 	bytes, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
 	if err != nil {
-		return "",err
+		return "", err
 	}
 
 	return string(bytes), nil
 }
 
-func CheckPassword(hashedPassword, password string)error{
+func CheckPassword(hashedPassword, password string) error {
 	return bcrypt.CompareHashAndPassword([]byte(hashedPassword), []byte(password))
 }
 
-
-func GenerateAccessToken(userID string)(string, error){
+func GenerateAccessToken(userID string) (string, error) {
 	claims := jwt.MapClaims{
 		"userId": userID,
-		"exp": time.Now().Add(30 * time.Minute).Unix(),
+		"exp":    time.Now().Add(30 * time.Minute).Unix(),
 	}
 
 	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
 	return token.SignedString([]byte(accessTokenSecretKey))
 }
 
-func GenerateRefreshToken(userID string) (string, error){
+func GenerateRefreshToken(userID string) (string, error) {
 	claims := jwt.MapClaims{
 		"userId": userID,
-		"exp": time.Now().Add(7*24*time.Hour).Unix(),
+		"exp":    time.Now().Add(7 * 24 * time.Hour).Unix(),
 	}
 
 	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
 	return token.SignedString([]byte(refreshTokenSecretKey))
 }
-
-func ParseAccessToken(tokenString string, secretKey []byte)(*jwt.Token, error){
-	token, err := jwt.Parse(tokenString, func(t *jwt.Token) (interface{}, error) {
-		if _, ok := toke
-	})
-}
-
-
-// func ParseToken(tokenStr string) (uint, error){
-// 	token, err := jwt.Parse(tokenStr, func(t *jwt.Token) (interface{}, error) {
-// 		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok{
-// 			return nil, errors.New("Invalid signing method")
-// 		}
-// 		return []byte(os.Getenv("JWT_SECRET_KEY")), nil
-// 	})
-
-// 	if err != nil || !token.Valid {
-// 		return 0, err
-// 	}
-
-// 	claims, ok := token.Claims.(jwt.MapClaims)
-// 	if !ok {
-// 		return 0, errors.New("Invalid claims")
-// 	}
-
-// 	return uint(claims["sub"].(float64)), nil
-// }
