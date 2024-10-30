2a616e6985791d14db0cea981c95d533626cf958
Christian Isingizwe <bizimanaisingizwechristian@gmail.com>
2024-10-30 10:09:16 +0200

    Some changes made

--- internal/handlers/authHandlers.go
+++ internal/handlers/authHandlers.go
@@ -2,11 +2,14 @@ package handlers
 
 import (
 	"encoding/json"
+	"fmt"
+	"go/token"
 	"net/http"
 
 	"github.com/ChristianIsingizwe/Go_zone/internal/models"
 	"github.com/ChristianIsingizwe/Go_zone/internal/services"
 	"github.com/ChristianIsingizwe/Go_zone/internal/types"
+	"github.com/dgrijalva/jwt-go"
 )
 
 func RegisterUserHandler(w http.ResponseWriter, r *http.Response){
@@ -43,4 +46,59 @@ func RegisterUserHandler(w http.ResponseWriter, r *http.Response){
 
 	w.WriteHeader(http.StatusCreated)
 	json.NewEncoder(w).Encode(map[string]string{"message": "User registered successfully"})
+}
+
+
+func LoginHandler(w http.ResponseWriter, r *http.Request){
+	var req types.LoginRequest
+
+	if err := json.NewDecoder(r.Body).Decode(&req); err != nil{
+		http.Error(w, "Invalid request payload", http.StatusBadRequest)
+		return 
+	}
+
+	var user models.User
+
+	if err := services.DB.Where("email = ? ", req.Email).First(&user).Error; err != nil {
+		http.Error(w, "User not found", http.StatusUnauthorized)
+		return
+	}
+
+	if !services.CheckPassword(req.Password, user.Password){
+		http.Error(w, "Incorrect password", http.StatusUnauthorized)
+		return 
+	}
+
+	accessToken, err := services.GenerateAccessToken(fmt.Sprint(user.ID))
+	if err != nil {
+		http.Error(w, "Failed to generate access token", http.StatusInternalServerError)
+		return 
+	}
+
+	refreshToken, err := services.GenerateRefreshToken(fmt.Sprint(user.ID))
+	if err != nil {
+		http.Error(w, "Failed to generate  refresh token", http.StatusInternalServerError )
+		return 
+	}
+
+	json.NewEncoder(w).Encode(types.TokenResponse{
+		AccessToken: accessToken,
+		RefreshToken: refreshToken,
+	})
+}
+
+
+func RefreshTokenHandler(w http.ResponseWriter, r *http.Request){
+	refreshToken := r.Header.Get("Authorization")
+	if refreshToken == "" {
+		http.Error(w, "Refresh token required", http.StatusUnauthorized)
+		return 
+	}
+
+	token, err := jwt.Parse(refreshToken, func(t *jwt.Token) (interface{}, error) {
+		if _,ok := token.Method.(*jwt.SigningMethodHS256); !ok {
+			return nil, http.ErrAbortHandler
+		}
+		
+	})
 }
