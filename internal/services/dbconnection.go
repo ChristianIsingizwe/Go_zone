0521b71e9a6150c9a9a9d061d3fcceacf6680c7f
Christian Isingizwe <bizimanaisingizwechristian@gmail.com>
2024-10-30 05:46:43 +0200

    Started the database connection

---
+++ internal/services/dbconnection.go
@@ -0,0 +1,24 @@
+package services
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+var DB *gorm.DB
+
+func connectToDatabase() {
+	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s dbport=%s sslmode=%s", os.Getenv("DB_HOST"), os.Getenv("DB_USER"), os.Getenv("DB_PASSWORD"), os.Getenv("DB_NAME"), os.Getenv("DB_PORT"), os.Getenv("DB_SSLMODE"))
+    database, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		log.Fatalf("Failed to connect to the database : %v", err)
+	}
+
+	if err := database.AutoMigrate(&User{}, &Session{}, &CartItem{}, &Order{}, &OrderItem{}, &Review{}); err != nil{
+		log.Fatalf("Failed to auto-migrate:  %v", err)
+	}
+	DB = database
+}
